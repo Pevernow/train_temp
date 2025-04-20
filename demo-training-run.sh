3bc860e95409b390a91ee8c51ac40389dfa13dc1
pevernow <3450354617@qq.com>
2025-04-20 13:20:09 +0800

    perf: 将训练策略从ddp更改为deepspeed_stage_2以提升性能
    将训练策略从`ddp`更改为`deepspeed_stage_2`，以优化训练过程中的性能。同时，将`GRAD_CP`参数从1改为0，以加快训练速度并减少VRAM的使用。

--- demo-training-run.sh
+++ demo-training-run.sh
@@ -28,7 +28,7 @@ PROJ_DIR="out/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE # set output folder
 M_BSZ="16" # takes ~9G VRAM here => reduce this to save VRAM, increase this for faster speed
 LR_INIT="6e-4"
 LR_FINAL="6e-5"
-GRAD_CP=1 # 1 => slower, save VRAM; 0 => faster, more VRAM
+GRAD_CP=0 # 1 => slower, save VRAM; 0 => faster, more VRAM
 EPOCH_SAVE=10 # save every 10 "miniepochs" (1 miniepoch = 40320 * ctx_len tokens) => decrease if your GPU is weak
 #
 #######################################################################################################################
@@ -47,5 +47,5 @@ python train.py --load_model "0" --wandb "Test" --proj_dir $PROJ_DIR --my_testin
  --num_nodes $N_NODE --micro_bsz $M_BSZ --n_layer $N_LAYER --n_embd $N_EMBD \
  --lr_init $LR_INIT --lr_final $LR_FINAL --warmup_steps 10 --beta1 0.9 --beta2 0.99 --adam_eps 1e-18 --data_type "binidx" --vocab_size 65536 \
  --weight_decay 0.001 --epoch_save $EPOCH_SAVE --head_size 64 \
- --accelerator gpu --devices $GPU_PER_NODE --precision bf16 --strategy ddp --grad_cp $GRAD_CP --enable_progress_bar True --ds_bucket_mb $DS_BUCKET_MB \
+ --accelerator gpu --devices $GPU_PER_NODE --precision bf16 --strategy deepspeed_stage_2 --grad_cp $GRAD_CP --enable_progress_bar True --ds_bucket_mb $DS_BUCKET_MB \
  --train_depth 5 --eval_depths 5
