5aa0997d157aa628f0a1abb872a10924e927b690
pevernow <3450354617@qq.com>
2025-05-02 21:26:24 +0800

    fix: 将vocab_size从50304更改为0以适配新的数据格式
    此次更改将`vocab_size`参数从50304调整为0，以适配新的数据格式。原值可能导致训练过程中出现错误，调整后确保训练脚本能够正确运行。

--- demo-training-prepare-v7-pile.sh
+++ demo-training-prepare-v7-pile.sh
@@ -20,7 +20,7 @@ PROJ_DIR="out/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE # set output folder
 #######################################################################################################################
 #
 python train.py --wandb "" --proj_dir $PROJ_DIR \
- --data_file "/mnt/nvme0n1/pile/pile_20B_tokenizer_text_document" --data_type "binidx" --vocab_size 50304 --my_testing $MODEL_TYPE \
+ --data_file "/mnt/nvme0n1/pile/pile_20B_tokenizer_text_document" --data_type "binidx" --vocab_size 0 --my_testing $MODEL_TYPE \
  --ctx_len $CTX_LEN --train_stage 1 --epoch_count 1 --epoch_begin 0 \
  --epoch_save 1 --weight_decay 0 --head_size 64 \
  --num_nodes 1 --micro_bsz 1 --n_layer $N_LAYER --n_embd $N_EMBD --my_exit_tokens 332115325534 --magic_prime 81082817 \
